67a711d211f6e053cceda9f957fbfcd27bfdf669
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-02 17:13:01 -0500

    [ADD] cors to backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require('cors')
 const PORT = process.env.PORT || 4000;
 
 const app = express();
 
 // middlewares
+app.use(cors())
 app.use(express.json())
 
 app.get("/", (req, res) => {
